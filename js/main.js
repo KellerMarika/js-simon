a19d41c751f50932575975d5cba75c93ba41e3ac
KellerMarika <marikakeller92@gmail.com>
2022-11-14 15:32:06 +0100

    variabili

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
+
+/************* ELEMENTI*****************/
+
+//bottone play
+const play_btn=document.querySelector("[name=play-btn]");
+//console.log(play_btn);
+//contenitore numeri random
+const randomNumberContainer_El= document.getElementById("random-number-container");
+//contenitore punteggio finale
+//console.log(randomNumberContainer_El);
+const scoreContainer_El=document.getElementById("score-container");
+//console.log(scoreContainer_El);
+
+const randomIANumbers=[];
+const randomIANumbersLenght=5;
+
+const userNumbers=[];
+const userCorrectNumbers=[];
+
+
+
 /*
-Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-Consigli del giorno:
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma. */
-
-
-/* 
 
 1)addeventlistner sul bottone : {
     a)genera IAarray numeri random.
-    b)crea elemento genitore html
+    b)recupera elemento genitore html
     b)crea elementi  figli 
     c) e figli.append IAarray[x] random creati 
     d)el genitore.append e figli
@@ -52,15 +63,25 @@ Consigli del giorno:
 
         //finito il ciclo tiro le somme
 
-        create score_El ()//3 pezzi
+        crea elementi figli di score_el ()//3 pezzi
         append
-        "hai indovinato"+ userCorrectChoiche.lenght + "numeri: "//1p
+        "hai indovinato"+ userCorrectnumber.lenght + "numeri: "//1p
 
-            while (i=userCorrectChoiche.lenght){
-            score_El.append( userCorrectChoiche[i] + ", ");//1p
+            while (i=userCorrectnumber.lenght){
+            score_El.append( userCorrectnumber[i] + ", ");//1p
             }
 
-            //se tutto funziona e avanza tempo aggiungere valutazione finale in base alla userCorrectChoiche.lenght
+            //se tutto funziona e avanza tempo aggiungere valutazione finale in base alla userCorrectnumber.lenght
+
+
+
+            Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
     }
 
 
